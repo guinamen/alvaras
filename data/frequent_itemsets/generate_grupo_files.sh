bb5a766f5b261a8e0fe1a3ccf55c3977c4647258
Guilherme Namen Pimenta <guilhermenamen@pbh.gov.br>
2024-11-12 16:10:40 -0300

    Generate frequent itemset file.

---
+++ data/frequent_itemsets/generate_grupo_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "@CONVERTED_FROM_TEXT"
+while IFS= read -r line
+do
+	IFS='|'
+	read -ra arrIN <<< "$line"
+	echo "@ITEM=${arrIN[0]}=${arrIN[1]}"
+
+done < <(echo "select distinct id, descricao from atividade inner join cnae_grupo on cnae_grupo.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-6) order by id;" | sqlite3 ../database.db)
+
+while IFS= read -r line2
+do
+	echo "${line2}"
+
+done < <(echo "select rtrim(replace(group_concat(DISTINCT id||'@!'), '@!,', ' '),'@!') from atividade inner join cnae_grupo on cnae_grupo.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-6) group by alvara ;" | sqlite3 ../database.db)
+
