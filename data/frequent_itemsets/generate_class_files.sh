ec7fe87066351a3ab536aa4616afa737cabb7031
Guilherme Namen Pimenta <guilhermenamen@pbh.gov.br>
2024-12-03 13:27:22 -0300

    Adjust sql

--- data/frequent_itemsets/generate_class_files.sh
+++ data/frequent_itemsets/generate_class_files.sh
@@ -7,11 +7,11 @@ do
 	read -ra arrIN <<< "$line"
 	echo "@ITEM=${arrIN[0]}=${arrIN[1]}"
 
-done < <(echo "select distinct id, descricao from atividade inner join cnae_classe on cnae_classe.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-4) order by id;" | sqlite3 ../database.db)
+done < <(echo "select distinct id, descricao from atividade inner join classe on classe.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-4) order by id;" | sqlite3 ../database.db)
 
 while IFS= read -r line2
 do
 	echo "${line2}"
 
-done < <(echo "select rtrim(replace(group_concat(DISTINCT id||'@!'), '@!,', ' '),'@!') from atividade inner join cnae_classe on cnae_classe.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-4) group by alvara ;" | sqlite3 ../database.db)
+done < <(echo "select rtrim(replace(group_concat(DISTINCT id||'@!'), '@!,', ' '),'@!') from atividade inner join classe on classe.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-4) group by alvara ;" | sqlite3 ../database.db)
 
--- data/frequent_itemsets/generate_files.sh
+++ data/frequent_itemsets/generate_files.sh
@@ -7,11 +7,11 @@ do
 	read -ra arrIN <<< "$line"
 	echo "@ITEM=${arrIN[0]}=${arrIN[1]}"
 
-done < <(echo "select distinct id, descricao from atividade inner join cnae on cnae.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-2) order by id;" | sqlite3 ../database.db)
+done < <(echo "select distinct id, descricao from atividade inner join subclasse on subclasse.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-2) order by id;" | sqlite3 ../database.db)
 
 while IFS= read -r line
 do
 	echo "${line}"
 
-done < <(echo "select GROUP_CONCAT(id,' ') from atividade inner join cnae on cnae.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-2) group by alvara ;" | sqlite3 ../database.db)
+done < <(echo "select GROUP_CONCAT(id,' ') from atividade inner join subclasse on subclasse.codigo = SUBSTR(atividade, 1,LENGTH(atividade)-2) group by alvara ;" | sqlite3 ../database.db)
 
